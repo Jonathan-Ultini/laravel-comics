2e76374559c8ec4fec4ea9141f2199080bb26672
Jonathan-Ultini <jonathanultini@gmail.com>
2024-10-22 17:55:06 +0200

    add comics, footer, header no styles

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,29 @@
 <header>
-  <h1>
-    Header
-  </h1>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="/path/to/logo.png" alt="DC Comics" style="width: 50px;">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link" href="#">Characters</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Comics</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Movies</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">TV</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Games</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Collectibles</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Videos</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Fans</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">News</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Shop</a></li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
 </header>
